6e6dfa618e2cf02ecf08186bdc513397c972c292
ingekn <133350400+ingekn@users.noreply.github.com>
2023-12-06 20:29:18 +0000

    Updated styling and set default query

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Dictionary</h1>
-        <Dictionary />
+        <Dictionary defaultQuery="sunset" />
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,34 +4,50 @@ import Results from "./Results";
 
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [query, setQuery] = useState("");
+export default function Dictionary(props) {
+  const [query, setQuery] = useState(props.defaultQuery);
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiKey = "1386aafaa966aa68e4520o87btc31531";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${query}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function load() {
+    setLoaded(true);
+    search();
   }
   function getQuery(event) {
     setQuery(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input type="search" onChange={getQuery} />
-          <button type="submit">
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={getQuery} className="form-control" />
+            {/* <button type="submit">
             <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
-        </form>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+          </button> */}
+          </form>
+          <div className="hint">
+            Suggested words: sunset, wine, yoga, forest, meditation
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
